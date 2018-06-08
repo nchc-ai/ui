20175c5ed5f2706cf398354c521aff74c458af53
Jimmy Chuang <ogre0403@gmail.com>
2018-06-08 12:05:35 +0800

    add input box

--- example/restapi/frontend/src/App.js
+++ example/restapi/frontend/src/App.js
@@ -8,13 +8,18 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      message: ''
+      message: '',
+      replica: '1',
+      msg: 'shit world!!!'
     }
 
     this.handleClick1 = this.handleClick1.bind(this)
     this.handleClick2 = this.handleClick2.bind(this)
     this.handleClick3 = this.handleClick3.bind(this)
     this.handleClick4 = this.handleClick4.bind(this)
+    this.updateInput = this.updateInput.bind(this);
+    this.updateInput1 = this.updateInput1.bind(this);
+
   }
 
 
@@ -30,33 +35,41 @@ class App extends Component {
   }
 
   handleClick3() {
-
     axios.post(
       '/createDeploy',
       {
-        replica: 3
+        replica: parseInt(this.state.replica)
       })
       .then(response => this.setState({message: response.data.message}))
-
-
   }
 
   handleClick4() {
     axios.post(
       '/createWorkflow',
       {
-        message: "shit world!!!"
+        message: this.state.msg
       })
       .then(response => this.setState({message: response.data.message}))
   }
 
+  updateInput(event) {
+    this.setState({replica: event.target.value})
+  }
+
+  updateInput1(event) {
+    this.setState({msg: event.target.value})
+  }
 
   render() {
     return (
       <div className="App">
 
+        <input type="text" onChange={this.updateInput}></input>
         <button onClick={this.handleClick3}>create deployment</button>
         <button onClick={this.handleClick1}>get deployment replica</button>
+        <br/>
+
+        <input type="text1" onChange={this.updateInput1}></input>
         <button onClick={this.handleClick4}>create workflow</button>
         <button onClick={this.handleClick2}>get workflow number</button>
 
