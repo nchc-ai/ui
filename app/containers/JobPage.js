d2d1adc3c3bb376eb2368a463adabd7fb00fd256
Toby Chung <toby4120@gmail.com>
2019-01-25 05:44:17 +0800

    feat(job): mock job data for testing

--- app/containers/JobPage.js
+++ app/containers/JobPage.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import { Row, Col } from 'reactstrap';
 import { notify } from 'react-notify-toast';
 import Clipboard from 'react-clipboard.js';
+import { doubleRawList } from '../mock/jobData';
 import { TOAST_TIMING } from '../constants';
 import bindActionCreatorHoc from '../libraries/bindActionCreatorHoc';
 import DataFrame from '../components/common/DataFrame/index';
@@ -122,44 +123,21 @@ class JobPage extends Component {
       userInfo
     } = this.props;
 
-    // const mockJobs = [
-    //   {
-    //     "course_id": "b86b2893-b876-45c2-a3f6-5e099c15d638",
-    //     "dataset": [
-    //       "cifar-10",
-    //       "mnist"
-    //     ],
-    //     "gpu": 1,
-    //     "id": "49a31009-7d1b-4ff2-badd-e8c717e2256c",
-    //     "image": "nvidia/caffe:latest",
-    //     "introduction": "markdown text with escape",
-    //     "level": "basic",
-    //     "name": "mage process",
-    //     "service": [
-    //       {
-    //         "label": "SSH",
-    //         "value": "http://140.110.5.22:30010"
-    //       }, {
-    //         "label": "VNC",
-    //         "value": "http://140.110.5.22:30010"
-    //       }, {
-    //         "label": "Share path",
-    //         "value": "http://140.110.5.22:30010"
-    //       }
-    //     ],
-    //     "startAt": "2018-06-25T09:24:38Z",
-    //     "status": "Ready"
-    //   }
+
+    const infoArr = [{
+      title: '容器課程',
+    }, {
+      title: 'vm課程',
+    }];
+
+    const doubleList = _.merge(doubleRawList, infoArr);
+
+    // const doubleList = [
+    //   ...this.props.container,
+    //   ...this.props.vm
     // ];
-    // const data = mockJobs;
-    // const data = Job.list;
 
-    const doubleList = [
-      ...this.props.container,
-      ...this.props.vm
-    ];
-
-
+    // console.log('singleList', doubleList);
 
     return (
       <div className="job-bg">
@@ -169,106 +147,108 @@ class JobPage extends Component {
         >
 
         {
-          doubleList.map( singleList => (
-            <DataFrame
-              isLoading={singleList.loading}
-              data={groupArray(data, 'name')}
-              cols={8}
-            >
-              {
-                groupArray(singleList.data, 'name').map(
-                  (obj, i) => (
-                    <div key={i} className="job-group">
-                      <Row className="title-row">
-                        <Col>
-                          <h4 className="fl">{obj.group}</h4>
-                          {/* <button className="fl btn-add" onClick={e => this.addJob(e, obj.data[0].courseId)}>+ 新增</button> */}
-                        </Col>
-                      </Row>
-                      <div>
-                        <DataFrame
-                          data={obj.data}
-                          cols={8}
-                        >
-                          <Row>
-                            {
-                              obj.data.map((thumb, j) => (
-                                <Col key={j} md={4}>
-                                  {/* options cover */}
-                                  <div
-                                    className={`job-card__mask ${ this.state.isOptionsOpen ? 'job-card__mask--open' : '' }`}
-                                    onMouseLeave={() => this.leaveCardOptions()}
-                                  >
-                                    <ul className="job-card__mask-options">
-                                      <li>
-                                        <Clipboard
-                                          option-text={this.getCopiedText}
-                                          onSuccess={this.onCopySuccess}
-                                        >
-                                          <FaCopy/>
-                                        </Clipboard>
-                                        <p>複製網址</p>
-                                      </li>
-                                    </ul>
-                                  </div>
-                                  {/* card */}
-                                  <div className="job-card">
+          doubleList.map( (singleList) => (
 
-                                    {/* delete button */}
-                                    <button className="btn-cancel" onClick={e => this.deleteJob(e, thumb)}>X</button>
-
-                                    {/* snapshot button */}
-                                    <button className="btn-camera" onClick={e => this.snapshotJob(e, thumb)}>
-                                      <FaCamera/>
-                                    </button>
-                                    <p className="job-card-status">
-                                      <span className={`light light-${thumb.status}`} />
-                                      <span className="status-word">
-                                        {formatStatus(thumb.status)}
-                                      </span>
-                                    </p>
-
-                                    <p className={`job-card-id ${ userInfo.role !== 'teacher' ? "job-card-id--disable" : null}`} >{thumb.id}</p>
-
-                                    <div className="job-card-link-li">
-                                      {
-                                        thumb.service ?
-                                        thumb.service.map(
-                                          (service, k) => (
-                                            <span key={k} className="job-card-link" onClick={this.openCardMask}>
-                                              {
-                                                service.label === 'Share path' ?
-                                                  <span onClick={e => this.openCardMask(e, service)} >{service.label}</span>
-                                                :
-                                                  <a
-                                                    href={service.value}
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                  >
-                                                    {service.label}
-                                                  </a>
-                                              }
-                                              <span className="divide-line">|</span>
-                                            </span>
-                                          )
-                                        ) : null
-                                      }
+            <div>
+              <h2>{singleList.title}</h2>
+              <DataFrame
+                isLoading={singleList.loading}
+                data={groupArray(singleList.data, 'name')}
+                cols={8}
+              >
+                {
+                  groupArray(singleList.data, 'name').map(
+                    (obj, i) => (
+                      <div key={i} className="job-group">
+                        <Row className="title-row">
+                          <Col>
+                            <h4 className="fl">{obj.group}</h4>
+                            {/* <button className="fl btn-add" onClick={e => this.addJob(e, obj.data[0].courseId)}>+ 新增</button> */}
+                          </Col>
+                        </Row>
+                        <div>
+                          <DataFrame
+                            data={obj.data}
+                            cols={8}
+                          >
+                            <Row>
+                              {
+                                obj.data.map((thumb, j) => (
+                                  <Col key={j} md={4}>
+                                    {/* options cover */}
+                                    <div
+                                      className={`job-card__mask ${ this.state.isOptionsOpen ? 'job-card__mask--open' : '' }`}
+                                      onMouseLeave={() => this.leaveCardOptions()}
+                                    >
+                                      <ul className="job-card__mask-options">
+                                        <li>
+                                          <Clipboard
+                                            option-text={this.getCopiedText}
+                                            onSuccess={this.onCopySuccess}
+                                          >
+                                            <FaCopy/>
+                                          </Clipboard>
+                                          <p>複製網址</p>
+                                        </li>
+                                      </ul>
                                     </div>
-                                    <div className="corner-triangle" />
-                                  </div>
-                                </Col>
-                              ))
-                            }
-                          </Row>
-                        </DataFrame>
-                      </div>
-                    </div>
-                  )
-                )
-              }
-            </DataFrame>
-          
+                                    {/* card */}
+                                    <div className="job-card">
 
+                                      {/* delete button */}
+                                      <button className="btn-cancel" onClick={e => this.deleteJob(e, thumb)}>X</button>
+
+                                      {/* snapshot button */}
+                                      <button className="btn-camera" onClick={e => this.snapshotJob(e, thumb)}>
+                                        <FaCamera/>
+                                      </button>
+                                      <p className="job-card-status">
+                                        <span className={`light light-${thumb.status}`} />
+                                        <span className="status-word">
+                                          {formatStatus(thumb.status)}
+                                        </span>
+                                      </p>
+
+                                      <p className={`job-card-id ${ userInfo.role !== 'teacher' ? "job-card-id--disable" : null}`} >{thumb.id}</p>
+
+                                      <div className="job-card-link-li">
+                                        {
+                                          thumb.service ?
+                                          thumb.service.map(
+                                            (service, k) => (
+                                              <span key={k} className="job-card-link" onClick={this.openCardMask}>
+                                                {
+                                                  service.label === 'Share path' ?
+                                                    <span onClick={e => this.openCardMask(e, service)} >{service.label}</span>
+                                                  :
+                                                    <a
+                                                      href={service.value}
+                                                      target="_blank"
+                                                      rel="noopener noreferrer"
+                                                    >
+                                                      {service.label}
+                                                    </a>
+                                                }
+                                                <span className="divide-line">|</span>
+                                              </span>
+                                            )
+                                          ) : null
+                                        }
+                                      </div>
+                                      <div className="corner-triangle" />
+                                    </div>
+                                  </Col>
+                                ))
+                              }
+                            </Row>
+                          </DataFrame>
+                        </div>
+                      </div>
+                    )
+                  )
+                }
+              </DataFrame>
+            </div>
           ))
         }
           </CommonPageContent>
---
+++ app/mock/jobData.js
@@ -0,0 +1,112 @@
+export const doubleRawList = [
+  {
+    key: 1,
+    loading: false,
+    data: [{
+      "course_id": "b86b2893-b876-45c2-a3f6-5e099c15d638",
+      "dataset": [
+        "cifar-10",
+        "mnist"
+      ],
+      "gpu": 1,
+      "id": "49a31009-7d1b-4ff2-badd-e8c717e2256c",
+      "image": "nvidia/caffe:latest",
+      "introduction": "markdown text with escape",
+      "level": "basic",
+      "name": "container process",
+      "service": [
+        {
+          "label": "jupyter",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "ssh",
+          "value": "http://140.110.5.22:30010"
+        }
+      ],
+      "startAt": "2018-06-25T09:24:38Z",
+      "status": "Ready"
+    }]
+  },  {
+    key: 2,
+    loading: false,
+    data: [{
+      "course_id": "b86b2893-b876-45c2-a3f6-5e099c15d638",
+      "dataset": [
+        "cifar-10",
+        "mnist"
+      ],
+      "gpu": 1,
+      "id": "49a31009-7d1b-4ff2-badd-e8c717e2256c",
+      "image": "nvidia/caffe:latest",
+      "introduction": "markdown text with escape",
+      "level": "basic",
+      "name": "vm process",
+      "service": [
+        {
+          "label": "SSH",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "VNC",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "Share path",
+          "value": "http://140.110.5.22:30010"
+        }
+      ],
+      "startAt": "2018-06-25T09:24:38Z",
+      "status": "Ready"
+    }, {
+      "course_id": "b86b2893-b876-45c2-a3f6-5e099c15d638",
+      "dataset": [
+        "cifar-10",
+        "mnist"
+      ],
+      "gpu": 1,
+      "id": "49a39-7d1b-4ff2-badd-e8c717e2256c",
+      "image": "nvidia/caffe:latest",
+      "introduction": "markdown text with escape",
+      "level": "basic",
+      "name": "vm process",
+      "service": [
+        {
+          "label": "SSH",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "VNC",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "Share path",
+          "value": "http://140.110.5.22:30010"
+        }
+      ],
+      "startAt": "2018-06-25T09:24:38Z",
+      "status": "Ready"
+    }, {
+      "course_id": "b86b2893-b876-45c2-a3f6-5e099c15d638",
+      "dataset": [
+        "cifar-10",
+        "mnist"
+      ],
+      "gpu": 1,
+      "id": "49a39-7d1b-4ff2-badd-e8c717e2256c",
+      "image": "nvidia/caffe:latest",
+      "introduction": "markdown text with escape",
+      "level": "basic",
+      "name": "vm process 2",
+      "service": [
+        {
+          "label": "SSH",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "VNC",
+          "value": "http://140.110.5.22:30010"
+        }, {
+          "label": "Share path",
+          "value": "http://140.110.5.22:30010"
+        }
+      ],
+      "startAt": "2018-06-25T09:24:38Z",
+      "status": "Ready"
+    }]
+  },
+]
