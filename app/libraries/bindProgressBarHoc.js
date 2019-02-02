0f4db76182d7869046565a81cc0ef8755e29f9b6
Toby Chung <toby4120@gmail.com>
2019-02-02 23:39:11 +0800

    refactor(common): modify hoc of progressbar

---
+++ app/libraries/bindProgressBarHoc.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function bindProgressBarHoc(WrappedComponent) {
+  class Wrapper extends React.Component {
+
+    componentWillUnmount() {
+      clearInterval(this.interval);
+      if(this.timer){
+        clearTimeout(this.timer);
+    }
+    }
+
+    startProgressBar = () => {
+      let progress = 0;
+      this.interval = setInterval(() => {
+        progress += parseInt(Math.random() * 4);
+        this.props.uiAction.toggleProgressBar({ toggle: true, progress });
+      }, 1400);
+    }
+
+    endPorgressBar = () => {
+      this.props.uiAction.toggleProgressBar({ toggle: true, progress: 100 });
+      clearInterval(this.interval);
+
+      this.timer = setTimeout(
+        function() {
+          this.props.uiAction.removeProgressBar();
+        }
+        .bind(this),
+        1000
+      );
+    }
+
+    render = () => (
+      <WrappedComponent {...this.props}
+        startProgressBar={this.startProgressBar}
+        endPorgressBar={this.endPorgressBar}
+      />);
+  }
+
+  return Wrapper;
+}
