1f257863f2befc93056a63b08a75305ee9124856
Toby Chung <toby4120@gmail.com>
2019-02-28 18:53:59 +0800

    refactor(listView): modify width of list view, #89

--- app/components/common/ListView/index.js
+++ app/components/common/ListView/index.js
@@ -16,19 +16,25 @@ const TableContainer = styled.div`
 `;
 
 
-
-const Label = styled.span`
-
+const Li = styled.div`
+  overflow: hidden;
+  width: 100%;
+  margin-bottom: ${props => props.type == 'multi-select' ? '0px' : '30px'};
 `;
+
 
 const Unit = styled.span`
   padding-left: 10px;
 `;
 
-const Value = styled.span`
-  display: inline-block;
+const ValueGroup = styled.div`
+  float: left;
+  width: 80%;
+`;
+
+const Value = styled.div`
+  float: left;
   padding-left: 20px;
-  line-height: 50px;
 `;
 
 const EmptyValue = styled(Value)`
@@ -36,18 +42,23 @@ const EmptyValue = styled(Value)`
 `
 
 const ListBullet = styled.img`
-  display: inline-block !important;
+  float: left;
   width: 20px;
   height: 20px;
-  margin-top: -5px;
+  margin-top: 3px;
   margin-right: 10px;
-  line-height: -40px;
 `;
 
-const ArrayItem = styled.span`
+const Label = styled.span`
+  float: left;
+`;
+
+const ArrayItem = styled.div`
+  float: left;
   background-color: #D3EBEB;
   margin-right: 20px;
-  padding: 5px 15px;
+  margin-bottom: 20px;
+  padding: 0px 15px;
   border-radius: 5px;
 `;
 
@@ -70,13 +81,13 @@ const ListView = ({ isLoading, templateData, detailData, size }) => {
       data={templateData}
       cols={8}
     >
-      <Row>
+      <div>
         {
           templateData.map(template => (
-            <Col
+            <Li
               key={template.key}
               md={{ size: size ? 12 / size : 12 }}
-              className={`list-view-li list-view-li-${template.name}`}
+              type={template.type}
             >
               {/* Bullet */}
               {
@@ -88,84 +99,86 @@ const ListView = ({ isLoading, templateData, detailData, size }) => {
               {/* Label */}
               <Label>{template.label}： </Label>
 
-              {/* Define different value here */}
-              {
-                template.type === 'text' ?
-                  <Value>{_.get(detailData, template.name, '尚無資料') || '尚無資料'}</Value>
-                : null
-              }
+              <ValueGroup>
+                {/* Define different value here */}
+                {
+                  template.type === 'text' ?
+                    <Value>{_.get(detailData, template.name, '尚無資料') || '尚無資料'}</Value>
+                  : null
+                }
 
-              {
-                template.type === 'number' ?
-                  <Value>{_.get(detailData, template.name, 0)}</Value>
-                : null
-              }
+                {
+                  template.type === 'number' ?
+                    <Value>{_.get(detailData, template.name, 0)}</Value>
+                  : null
+                }
 
-              {
-                template.type === 'single-select' ?
-                  <Value>{_.get(detailData, `${template.name}.label`, '尚無資料') || '尚無資料'}</Value>
-                : null
-              }
-              {
-                template.type === 'multi-select' ?
-                  <Value>
-                    {
-                      _.get(detailData, template.name, []).length > 0 ?
-                        detailData[template.name].map((datum, index) => (
-                          <ArrayItem key={`array-${index}`}>{_.get(datum, "label", '')}</ArrayItem>
-                        ))
-                      : <EmptyValue>尚無資料</EmptyValue>
-                    }
-                  </Value>
-                : null
-              }
-              {
-                template.type === 'html' ?
-                  <Value>{decodeHtml(detailData[template.name])}</Value>
-                : null
-              }
+                {
+                  template.type === 'single-select' ?
+                    <Value>{_.get(detailData, `${template.name}.label`, '尚無資料') || '尚無資料'}</Value>
+                  : null
+                }
+                {
+                  template.type === 'multi-select' ?
+                    <Value>
+                      {
+                        _.get(detailData, template.name, []).length > 0 ?
+                          detailData[template.name].map((datum, index) => (
+                            <ArrayItem key={`array-${index}`}>{_.get(datum, "label", '')}</ArrayItem>
+                          ))
+                        : <EmptyValue>尚無資料</EmptyValue>
+                      }
+                    </Value>
+                  : null
+                }
+                {
+                  template.type === 'html' ?
+                    <Value>{decodeHtml(detailData[template.name])}</Value>
+                  : null
+                }
 
-              {
-                template.type === 'markdown' ?
-                <ReactMarkdown source={_.get(targetForm, template.name, '尚無資料')} />
-                : null
-              }
+                {
+                  template.type === 'markdown' ?
+                  <ReactMarkdown source={_.get(targetForm, template.name, '尚無資料')} />
+                  : null
+                }
 
-              {
-                template.type === 'ports' ?
-                  <Value>
-                    {
-                      _.get(detailData, template.name, []).length > 0 ?
-                        detailData[template.name].map((datum, index) => (
-                          <ArrayItem key={`array-${index}`}>
-                            {`${_.get(datum, 'name', '')} : ${_.get(datum, 'port', '')}`}
-                          </ArrayItem>
-                        ))
-                      : <EmptyValue>尚無資料</EmptyValue>
-                    }
-                  </Value>
-                : null
-              }
-              {
-                template.type === 'boolean' ?
-                  <Value>
-                    { _.get(detailData, template.name) === true || _.get(detailData, template.name) === 'true' ?
-                      `${_.get(template, 'custom.trueText', '是')}`
-                      :
-                      `${_.get(template, 'custom.falseText', '否')}`
-                    }
-                  </Value>
-                : null
-              }
-              {
-                template.unit && _.get(detailData, template.name, "") || _.get(detailData, template.name, "") === 0 ?
-                  <Unit>{ template.unit }</Unit>
-                : null
-              }
-            </Col>
+                {
+                  template.type === 'ports' ?
+                    <Value>
+                      {
+                        _.get(detailData, template.name, []).length > 0 ?
+                          detailData[template.name].map((datum, index) => (
+                            <ArrayItem key={`array-${index}`}>
+                              {`${_.get(datum, 'name', '')} : ${_.get(datum, 'port', '')}`}
+                            </ArrayItem>
+                          ))
+                        : <EmptyValue>尚無資料</EmptyValue>
+                      }
+                    </Value>
+                  : null
+                }
+                {
+                  template.type === 'boolean' ?
+                    <Value>
+                      { _.get(detailData, template.name) === true || _.get(detailData, template.name) === 'true' ?
+                        `${_.get(template, 'custom.trueText', '是')}`
+                        :
+                        `${_.get(template, 'custom.falseText', '否')}`
+                      }
+                    </Value>
+                  : null
+                }
+                {
+                  template.unit && _.get(detailData, template.name, "") || _.get(detailData, template.name, "") === 0 ?
+                    <Unit>{ template.unit }</Unit>
+                  : null
+                }
+              </ValueGroup>
+            </Li>
           ))
         }
-      </Row>
+      </div>
     </DataFrame>
   </Comp>
 )};
