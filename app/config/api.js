678fde7fd9f31b3bdb89a8c0ee6d041bd238766e
Jimmy Chuang <ogre0403@gmail.com>
2019-03-04 15:04:05 +0800

    update server ip configuration

--- app/config/api.js
+++ app/config/api.js
@@ -1,9 +1,9 @@
 export const WEBSITE_URL = 'http://localhost:3010';
 
 // export const API_URL = 'http://140.110.5.22:30003';
-export const API_URL = 'http://140.110.16.191:38080/api';
+export const API_URL = 'http://140.110.18.106:38080/api';
 
-export const API_VM_URL = 'http://140.110.16.191:8085'
+export const API_VM_URL = 'http://140.110.18.1061:8085'
 
 export const API_MOCK_URL = 'http://localhost:3000'
 
@@ -11,7 +11,7 @@ export const API_VERSION = 'beta';
 
 export const API_VM_VERSION = 'v1';
 
-export const AUTH_PROVIDER_URL = 'http://140.110.16.191:30080';
+export const AUTH_PROVIDER_URL = 'http://140.110.18.106:30080';
 
 export const RETURN_ROUTE = '/user/classroom-manage';
 
