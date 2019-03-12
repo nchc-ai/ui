9b8be75cbe6406d3387ef7e010766882f00224c4
Toby Chung <toby4120@gmail.com>
2019-03-12 10:15:47 +0800

    feat(dataFrame): optimize status in data frame

--- app/components/common/DataFrame/Loading.js
+++ app/components/common/DataFrame/Loading.js
@@ -1,11 +1,32 @@
 import React from "react";
-import loadingGIF from "../../../../public/images/common/loading.gif";
+import styled from 'styled-components';
+import MDSpinner from 'react-md-spinner';
+
+const Comp = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: inline-grid;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  width: 80px;
+  height: 80px;
+  padding-top: 30px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
 
 const Loading = () => (
-  <div className="loading-comp">
-    <span className="v-helper" />
-    <img alt="" src={loadingGIF} />
-  </div>
+  <Comp>
+    <Container>
+      <MDSpinner size={32} />
+    </Container>
+  </Comp>
 );
 
 export default Loading;
--- app/components/common/DataFrame/NoData.js
+++ app/components/common/DataFrame/NoData.js
@@ -1,15 +1,44 @@
 import React from "react";
-import { IconContext, FaGhost } from "react-icons/fa";
+import { Browser } from 'react-kawaii';
+import styled from 'styled-components';
+
+
+const Comp = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  width: 200px;
+  height: initial;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-direction:column;
+  text-align: center;
+  color:#9C9C9C;
+`
+
+const IconContainer = styled.div`
+  padding-top: 20px;
+  font-size: 45px;
+`
+
+const Word = styled.h5`
+  line-height: 40px;
+`
+
+
 
 const NoData = ({ emptyWord }) => (
-  <div className="no-data-comp">
-    <div className="no-data-container">
-      <div className="icon-container">
-        <FaGhost />
-      </div>
-      <h5 className="no-data-word">{emptyWord}</h5>
-    </div>
-  </div>
+  <Comp>
+    <Container>
+      <IconContainer>
+        <Browser size={50} mood="shocked" color="#61DDBC" />
+      </IconContainer>
+      <Word className="no-data-word">{emptyWord}</Word>
+    </Container>
+  </Comp>
 );
 
 export default NoData;
