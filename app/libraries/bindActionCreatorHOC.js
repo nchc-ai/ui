8ba509d14e0d9b5c6c558a0f6d2364c934a089b9
Jimmy Chuang <ogre0403@gmail.com>
2019-03-12 21:48:57 +0800

    (fix:) modify HOC file name to upper case to fix build fail in Linux

---
+++ app/libraries/bindActionCreatorHOC.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as Auth from '../actions/Auth';
+import * as Classroom from '../actions/Classroom';
+import * as Course from '../actions/Course';
+import * as Job from '../actions/Job';
+import * as Role from '../actions/Role';
+import * as Ui from '../actions/Ui';
+
+export default function bindActionCreatorHOC(WrappedComponent) {
+  class Wrapper extends React.Component {
+    render = () => (<WrappedComponent {...this.props} />);
+  }
+
+  const mapDispatchToProps = dispatch => ({
+    authAction: bindActionCreators(Auth, dispatch),
+    roomAction: bindActionCreators(Classroom, dispatch),
+    courseAction: bindActionCreators(Course, dispatch),
+    jobAction: bindActionCreators(Job, dispatch),
+    roleAction: bindActionCreators(Role, dispatch),
+    uiAction: bindActionCreators(Ui, dispatch)
+  });
+  return connect(null, mapDispatchToProps)(Wrapper);
+}
